23d5cce03ee8b2019f3c45e0be3bf0d641bc96f9
Nikita <nikitakorch08@gmail.com>
2026-06-09 19:15:20 +0200

    Add files via upload

--- Limunada.js
+++ Limunada.js
@@ -1,10 +1,10 @@
 const fillN = document.getElementById("fillN");
 const fillM = document.getElementById("fillM");
 let percentN = 40;
-let percentM = 60;
+let percentM = 43.33;
 
 fillN.style.width = `${percentN}%`;
 fillN.textContent = `${percentN}% - ${65000 * (percentN / 100)}`;
 
 fillM.style.width = `${percentM}%`;
-fillM.textContent = `${percentM}% - ${55150 * (percentM / 100)}`;
+fillM.textContent = `${percentM}% - ${60000 * (percentM / 100)}`;
