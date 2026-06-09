95c7d387bf6ba0b4dfed169b8f2de4175e09d5fc
Nikita <nikitakorch08@gmail.com>
2026-06-09 19:43:34 +0200

    Update moneyM value and adjust fillM calculations

--- Limunada.js
+++ Limunada.js
@@ -1,7 +1,7 @@
 const fillN = document.getElementById("fillN");
 const fillM = document.getElementById("fillM");
 let moneyN = 26000;
-let moneyM = 26000;
+let moneyM = 24000;
 let needN = 65000;
 let needM = 60000;
 let percentN = Math.floor((moneyN / needN) * 100);
