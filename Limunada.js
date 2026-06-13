3cde4aac12898b50b3ce6cd0873b48791810bca8
Nikita <nikitakorch08@gmail.com>
2026-06-13 10:28:50 +0200

    Add files via upload

--- Limunada.js
+++ Limunada.js
@@ -8,3 +8,22 @@ fillN.style.width = `${percentN}%`;
 fillN.textContent = `~${percentN}% - ~${moneyN} din`;
 
 need.textContent = `~${needN} din`;
+
+const themeBtn = document.getElementById('themeToggle');
+
+if (localStorage.getItem('theme') === 'dark') {
+  document.body.classList.add('dark-theme');
+  themeBtn.textContent = '☀️';
+}
+
+themeBtn.addEventListener('click', () => {
+  document.body.classList.toggle('dark-theme');
+
+  if (document.body.classList.contains('dark-theme')) {
+    themeBtn.textContent = '☀️';
+    localStorage.setItem('theme', 'dark');
+  } else {
+    themeBtn.textContent = '🌙';
+    localStorage.setItem('theme', 'light');
+  }
+});
