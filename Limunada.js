467e225474774ac28105aad8f5d774197affacc5
Nikita <nikitakorch08@gmail.com>
2026-06-14 19:47:37 +0200

    Add files via upload

--- Limunada.js
+++ Limunada.js
@@ -1,6 +1,6 @@
 const fillN = document.getElementById('fillN');
 const need = document.querySelector('.howMuch');
-let moneyN = 26000;
+let moneyN = 26710;
 let needN = 70000;
 let percentN = Math.floor((moneyN / needN) * 100);
 
